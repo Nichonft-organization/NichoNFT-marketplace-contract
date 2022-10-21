2d33b38ff834ccf2118b78135819c8630cc18f56
monkeyMax-SVG <monkeymax@yeah.net>
2022-10-21 16:19:54 +0800

    deployed final version for testnet

--- README.md
+++ README.md
@@ -9,13 +9,13 @@
 [```0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E```](https://testnet.bscscan.com/address/0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E)
 
 ### NichoNFT 
-[```0x3bdec7B3A75A150C7Bc7aa4a93a524bDB733F91e```](https://testnet.bscscan.com/address/0x3bdec7B3A75A150C7Bc7aa4a93a524bDB733F91e)
+[```0x924384699f32af9B9aE33707BDB82b7DB755578a```](https://testnet.bscscan.com/address/0x924384699f32af9B9aE33707BDB82b7DB755578a)
 
 ### NichoNFTMarketplace 
-[```0x2651B0EDec2a2f68F730F82f20914d51158e9A09```](https://testnet.bscscan.com/address/0x2651B0EDec2a2f68F730F82f20914d51158e9A09)
+[```0x2B50917a7B6e007910DC0eD95075Db9774dCf674```](https://testnet.bscscan.com/address/0x2B50917a7B6e007910DC0eD95075Db9774dCf674)
 
 ### NichoNFTAuction 
-[```0x5B6A0aFd837424264f4Bdb64cF7ACbb78016b246```](https://testnet.bscscan.com/address/0x5B6A0aFd837424264f4Bdb64cF7ACbb78016b246)
+[```0xDe095d4590A8e9c575FcCC70985fA3F39e7346A7```](https://testnet.bscscan.com/address/0xDe095d4590A8e9c575FcCC70985fA3F39e7346A7)
 
 ### CollectionFactory 
-[```0xEb66552af4189CA97fb590Ab508fEc765091E254```](https://testnet.bscscan.com/address/0xEb66552af4189CA97fb590Ab508fEc765091E254)
+[```0x993Dead7179BeF441cbecE24A465bAfA2eabdD48```](https://testnet.bscscan.com/address/0x993Dead7179BeF441cbecE24A465bAfA2eabdD48)
--- verify.sh
+++ verify.sh
@@ -2,15 +2,15 @@
 # npx hardhat verify --network testnet 0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E
 
 # NichoNFT ()
-# npx hardhat verify --network testnet 0x3bdec7B3A75A150C7Bc7aa4a93a524bDB733F91e
+# npx hardhat verify --network testnet 0x924384699f32af9B9aE33707BDB82b7DB755578a
 
 # NichoNFTMarketplace (_blacklist, _nichonft)
-npx hardhat verify --network testnet 0x2651B0EDec2a2f68F730F82f20914d51158e9A09 "0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E" "0x3bdec7B3A75A150C7Bc7aa4a93a524bDB733F91e"
+npx hardhat verify --network testnet 0x2B50917a7B6e007910DC0eD95075Db9774dCf674 "0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E" "0x924384699f32af9B9aE33707BDB82b7DB755578a" "0x993Dead7179BeF441cbecE24A465bAfA2eabdD48"
 
 # NichoNFTAuction (_blacklist, _nichonftmarketplace)
-npx hardhat verify --network testnet 0x5B6A0aFd837424264f4Bdb64cF7ACbb78016b246 "0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E" "0x2651B0EDec2a2f68F730F82f20914d51158e9A09"
+npx hardhat verify --network testnet 0xDe095d4590A8e9c575FcCC70985fA3F39e7346A7 "0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E" "0x2B50917a7B6e007910DC0eD95075Db9774dCf674" "0x993Dead7179BeF441cbecE24A465bAfA2eabdD48"
 # CollectionFactory (_nichonftmarketplace)
-npx hardhat verify --network testnet 0xEb66552af4189CA97fb590Ab508fEc765091E254 "0x2651B0EDec2a2f68F730F82f20914d51158e9A09" 
+npx hardhat verify --network testnet 0x993Dead7179BeF441cbecE24A465bAfA2eabdD48
 
 #0xd296628e818fbccad52e615181eb53f667e43a48
-# npx hardhat verify --network testnet 0xd296628e818fbccad52e615181eb53f667e43a48 "0x4a3cf8ea5ef071d61b2d5d53757a0c0cda687860" "0x2651B0EDec2a2f68F730F82f20914d51158e9A09" "Test" "Test"
+# npx hardhat verify --network testnet 0xd296628e818fbccad52e615181eb53f667e43a48 "0x4a3cf8ea5ef071d61b2d5d53757a0c0cda687860" "0x2B50917a7B6e007910DC0eD95075Db9774dCf674" "Test" "Test" "100"
