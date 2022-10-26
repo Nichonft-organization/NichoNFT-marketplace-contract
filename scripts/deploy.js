e835f17cd42e2611b60458d6b10a437ed419b7e3
stanleyliu20 <72141776+stanleyliu20@users.noreply.github.com>
2022-10-26 21:57:12 +0800

    New deploy for all contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,19 +9,19 @@ const initialSupplyWei = ethers.utils.parseEther(initialSupply);
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  /*
+
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", ethers.utils.formatEther(await deployer.getBalance()).toString());
-  
+
   // Deploy NFTBlackList contract
   const NFTBlackList = await ethers.getContractFactory("NFTBlackList");
   const NFTBlackListContract = await NFTBlackList.deploy();
   await NFTBlackListContract.deployed();
   console.log("NFTBlackList", NFTBlackListContract.address)
 
-  */
 
-  const NFTBlackListContractaddress = "0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E";
+
+  //const NFTBlackListContractaddress = "0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E";
 
   // Deploy NichoNFT contract
   const NichoNFT = await ethers.getContractFactory("NichoNFT");
@@ -32,15 +32,15 @@ async function main() {
   // Deploy Collection contract
   const CollectionFactory = await ethers.getContractFactory("CollectionFactory");
   const CollectionFactoryContract = await CollectionFactory.deploy();
-  await CollectionFactoryContract.deployed();        
+  await CollectionFactoryContract.deployed();
   console.log("CollectionFactory", CollectionFactoryContract.address)
 
   // Deploy NichoNFT Marketplace contract
   const NichoNFTMarketplace = await ethers.getContractFactory("NichoNFTMarketplace");
   const NichoNFTMarketplaceContract = await NichoNFTMarketplace.deploy(
-      NFTBlackListContractaddress,
-      NichoNFTContract.address,
-      CollectionFactoryContract.address
+    NFTBlackListContract.address,
+    NichoNFTContract.address,
+    CollectionFactoryContract.address
   );
   await NichoNFTMarketplaceContract.deployed();
   console.log("NichoNFTMarketplace", NichoNFTMarketplaceContract.address)
@@ -48,9 +48,9 @@ async function main() {
   // Deploy NichoNFT auction contract
   const NichoNFTAuction = await ethers.getContractFactory("NichoNFTAuction");
   const NichoNFTAuctionContract = await NichoNFTAuction.deploy(
-      NFTBlackListContractaddress,
-      NichoNFTMarketplaceContract.address,
-      CollectionFactoryContract.address
+    NFTBlackListContract.address,
+    NichoNFTMarketplaceContract.address,
+    CollectionFactoryContract.address
   );
   await NichoNFTAuctionContract.deployed();
   console.log("NichoNFTAuction", NichoNFTAuctionContract.address)
@@ -61,12 +61,12 @@ async function main() {
   await CollectionFactoryContract.setMarketplaceContract(NichoNFTMarketplaceContract.address);
 
   // set reward
-  const reward = "0xEA0DA51F25938dD21B2181cC84674002E153Aa1c";
-  await NichoNFTContract.setRewardsContract(reward);
-  await NichoNFTContract.setMintRewardsEnable(true);
+  // const reward = "0x87084Bf1585218295a957c9cAec7447925A38B59";
+  // await NichoNFTContract.setRewardsContract(reward);
+  // await NichoNFTContract.setMintRewardsEnable(true);
 
-  await NichoNFTMarketplaceContract.setRewardsContract(reward);
-  await NichoNFTMarketplaceContract.setTradeRewardsEnable(true);
+  // await NichoNFTMarketplaceContract.setRewardsContract(reward);
+  // await NichoNFTMarketplaceContract.setTradeRewardsEnable(true);
   // Deploy new NFT contract and get that contract object
   // await CollectionFactoryContract.deploy("Test NFT", "TestNFT", { value: feePriceWei });            
   // const deployedCollectionAddress = await CollectionFactoryContract.getCreatorContractAddress(
@@ -77,8 +77,8 @@ async function main() {
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- verify.sh
+++ verify.sh
@@ -1,16 +1,16 @@
-# NFTBlackList ()
-# npx hardhat verify --network testnet 0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E
+#NFTBlackList ()
+npx hardhat verify --network testnet 0x8402c3A22385FD760F9b5E417763318Cbc162520 
 
 # NichoNFT ()
-# npx hardhat verify --network testnet 0xbBfA39e0483af0DeC1b07A3A4FdB27EEB34B8E19
+npx hardhat verify --network testnet 0x191837C0D498F1e506CDb5FE81596AE56aFEe71d 
 
-# NichoNFTMarketplace (_blacklist, _nichonft)
-npx hardhat verify --network testnet 0xAC4153304102bAA1Aa2DED5721aB34F3F7D4322E "0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E" "0xbBfA39e0483af0DeC1b07A3A4FdB27EEB34B8E19" "0x3D6E37786abA862F3bE52F060975e39541e6B50A"
+# NichoNFTMarketplace (_blacklist, _nichonft, factory)
+npx hardhat verify --network testnet 0xc39f6AE018f4328B1e98b9F72106bfE49e40FE14  "0x8402c3A22385FD760F9b5E417763318Cbc162520" "0x191837C0D498F1e506CDb5FE81596AE56aFEe71d" "0xC3aC6258900619409E0102d06566C1b4E3b3f483"
 
-# NichoNFTAuction (_blacklist, _nichonftmarketplace)
-npx hardhat verify --network testnet 0x674D5b6C1d3472100F7162D2Cf643B9e0ec08b07 "0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822b1E" "0xAC4153304102bAA1Aa2DED5721aB34F3F7D4322E" "0x3D6E37786abA862F3bE52F060975e39541e6B50A"
+# NichoNFTAuction (_blacklist, _nichonftmarketplace, factory)
+npx hardhat verify --network testnet 0xCD4F8793ce04bDeF0c83727a92112eAf35eCF769  "0x8402c3A22385FD760F9b5E417763318Cbc162520" "0xc39f6AE018f4328B1e98b9F72106bfE49e40FE14" "0xC3aC6258900619409E0102d06566C1b4E3b3f483"
 # CollectionFactory (_nichonftmarketplace)
-npx hardhat verify --network testnet 0x3D6E37786abA862F3bE52F060975e39541e6B50A
+npx hardhat verify --network testnet 0xC3aC6258900619409E0102d06566C1b4E3b3f483
 
 #0xd296628e818fbccad52e615181eb53f667e43a48
 # npx hardhat verify --network testnet 0x907dc3a77a6d85b37b70c37772a1172675921ae1 "0x4a3cf8ea5ef071d61b2d5d53757a0c0cda687860" "0xAC4153304102bAA1Aa2DED5721aB34F3F7D4322E" "RoyalTest" "ROYALTEST" "100"
